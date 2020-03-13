f9f78300050d2a1c2cd9a2d4b16e6c180f68ffc9
Steve Popoola <stevepop@gmail.com>
2020-03-13 21:26:49 +0000

    Added ability to store terraform state in S3 and DynamoDB

---
+++ state_storage/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+
+resource "aws_s3_bucket" "accountancycloud-terraform-state" {
+  bucket = "accountancycloud-terraform-state"
+
+  # prevent accidental deletion of this S3 bucket
+#   lifecycle {
+#       prevent_destroy = true
+#   }
+
+  # Enable versioning so we can see the full revision history of our state files
+  versioning {
+      enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+      rule {
+          apply_server_side_encryption_by_default {
+              sse_algorithm = "AES256"
+          }
+      }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "tac-db-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
+# terraform {
+#     backend "s3" {
+#         bucket = "accountancycloud-terraform-state"
+#         key = "global/s3/terraform.tfstate"
+#         region = "eu-west-2"
+#         dynamodb_table = "tac-db-locks"
+#         encrypt = true
+#     }
+# }
+
+# output "s3_bucket_arn" {
+#   value = "aws_s3_bucket.terraform_state.arn"
+#   description = "The ARN of the S3 bucket"
+# }
+
+
+# output "dynamodb_table_name" {
+#   value = "aws_s3_bucket.terraform_state.arn"
+#   description = "The name of the DynamoDB table"
+# }
+
+
